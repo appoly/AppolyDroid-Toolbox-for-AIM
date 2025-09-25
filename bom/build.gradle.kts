b0bd82f8a16f598a30f3f87e17c4490b2659c3ca
Bradley Duck <bradley@appoly.co.uk>
2025-09-25 11:04:06 +0100

    - Merge update from main library

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -13,28 +13,28 @@ dependencies {
 	// Define constraints for all AppolyDroid modules
 	constraints {
 		// Core modules
-		api("com.github.appoly.AppolyDroid-Toolbox:BaseRepo:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:BaseRepo-S3Uploader:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:BaseRepo-Paging:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:BaseRepo:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:BaseRepo-S3Uploader:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:BaseRepo-Paging:${libs.versions.toolboxVersion.get()}")
 
 		// UI State modules
-		api("com.github.appoly.AppolyDroid-Toolbox:UiState:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:AppSnackBar:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:AppSnackBar-UiState:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:UiState:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:AppSnackBar:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:AppSnackBar-UiState:${libs.versions.toolboxVersion.get()}")
 
 		// Date/Time modules
-		api("com.github.appoly.AppolyDroid-Toolbox:DateHelperUtil:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:DateHelperUtil-Room:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:DateHelperUtil-Serialization:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:DateHelperUtil:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:DateHelperUtil-Room:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:DateHelperUtil-Serialization:${libs.versions.toolboxVersion.get()}")
 
 		// Compose & Pagination modules
-		api("com.github.appoly.AppolyDroid-Toolbox:ComposeExtensions:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:LazyListPagingExtensions:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:LazyGridPagingExtensions:${libs.versions.toolboxVersion.get()}")
-		api("com.github.appoly.AppolyDroid-Toolbox:PagingExtensions:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:ComposeExtensions:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:LazyListPagingExtensions:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:LazyGridPagingExtensions:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:PagingExtensions:${libs.versions.toolboxVersion.get()}")
 
 		// S3 & Utility modules
-		api("com.github.appoly.AppolyDroid-Toolbox:S3Uploader:${libs.versions.toolboxVersion.get()}")
+		api("com.github.appoly.AppolyDroid-Toolbox-for-AIM:S3Uploader:${libs.versions.toolboxVersion.get()}")
 	}
 }
 
